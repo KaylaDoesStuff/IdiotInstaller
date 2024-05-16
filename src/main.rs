644c1896e5f2c0811b3a448fddbcd133785a46dc
KaylaDoesStuff <kayla.trevena982@gmail.com>
2024-05-16 17:45:24 -0400

    code added

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,51 @@
 use std::io;
 use std::process::Command;
 
-const base: [&str; 7] = ["Arch", "Debian", "RHEL", "Gentoo", "OpenSUSE", "Void", "FreeBSD"]
+const base: [&str; 7] = ["Arch", "Debian", "RHEL", "Gentoo", "OpenSUSE", "Void", "FreeBSD"];
 const types: [&str; 3] = ["File Managers","Browsers","Extra"];
-const file_managers: [&str; 4] = ["Caja", "Thunar", "Dolphin", "Nautilus"]
-
-struct install_client {
-    base: i32, 
-    to_install: [],
-
-}
+const file_managers: [&str; 4] = ["Caja", "Thunar", "Dolphin", "Nautilus"];
 
 fn main() {
     println!("What do you want to install?");
     for i in types {
-        println!("{i}) {:?}", types.i);
+        println!("{i}\n");
     }
-    let selection = io::stdin().read_line();
-    match selection {
-        (1) => get_selection("files");
-    }
+    let sel_choice: String = "";
+    let selection = io::stdin().read_line(&mut sel_choice);
+    match sel_choice {
+        Ok(1) => get_selection("files"),
+        Ok(2) => get_selection("browsers"),
+        Ok(3) => get_selection("base")
+    };
 }
 
-fn commands() {
-    let mut mkdir = Command::new("mkdir");
-    let mut pacman = Command::new("pacman");
-    let mut apt = Command::new("apt");
-    let mut rpm = Command::new("rpm");
+fn commands(command: &str, input: &str, input2: &str) {
+    let mkdir = Command::new("mkdir")
+        .arg("{input}")
+        .arg("{input2}");
+    let pacman = Command::new("pacman")
+        .arg("{input}")
+        .arg("{input2}");
+    let apt = Command::new("apt")
+        .arg("{input}")
+        .arg("{input2}");
+    let rpm = Command::new("rpm")
+        .arg("{input}")
+        .arg("{input2}");
 }
 
 fn get_selection(install_type: &str) -> String {
     println!("Please select a number");
     let i = 0;
     match install_type {
-        ("files") => for i in file_managers {
-            println!("{i}) {:?}", file_managers.i)
+        "files" => for i in file_managers {
+            println!("{i}\n")
         }
-        ("browsers") => for i in browsers {
-            println!("{i}) {:?}", browsers.i)
+        "browsers" => for i in browsers {
+            println!("{i}\n")
         }
-        ("base") => for i in base {
-            println!("{i}) {:?}", base.i)
+        "base" => for i in base {
+            println!("{i}\n")
         }
     }
 
