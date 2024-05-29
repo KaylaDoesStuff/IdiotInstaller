fa4fcf06fee15e52c61284537c663f057f4b551c
KaylaDoesStuff <kayla.trevena982@gmail.com>
2024-05-29 13:53:24 +1200

    Update command_funcs.rs

--- src/command_funcs.rs
+++ src/command_funcs.rs
@@ -1,13 +1,14 @@
- mod consts;
+ mod constant_vars;
+ mod main;
  
  pub fn check_distro() -> String {
     let which = "which".to_string();
     let mut distro = "Unknown";
-    for (i, pacman) in consts::PACMANS.iter().enumerate() {
-        let check = commandInput(which.clone(), pacman.to_string());
-        if check.contains(consts::PACMANS_OUTPUTS[i]) {
+    for (i, pacman) in constant_vars::PACMANS.iter().enumerate() {
+        let check = main::commandInput(which.clone(), pacman.to_string());
+        if check.contains(constant_vars::PACMANS_OUTPUTS[i]) {
             let sel = &mut distro;
-            *sel = consts::BASE[i];
+            *sel = constant_vars::BASE[i];
             break;
         }
     }
