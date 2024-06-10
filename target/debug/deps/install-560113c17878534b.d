ba5e35e6a1f5d893342af087c5b89566c6cf531c
KaylaDoesStuff <kayla.trevena982@gmail.com>
2024-06-10 22:30:33 +1200

    stuff

---
+++ target/debug/deps/install-560113c17878534b.d
@@ -0,0 +1,7 @@
+/home/kayla/Documents/GitHub/IdiotInstaller/target/debug/deps/libinstall-560113c17878534b.rmeta: src/main.rs src/command_funcs.rs src/constant_vars.rs
+
+/home/kayla/Documents/GitHub/IdiotInstaller/target/debug/deps/install-560113c17878534b.d: src/main.rs src/command_funcs.rs src/constant_vars.rs
+
+src/main.rs:
+src/command_funcs.rs:
+src/constant_vars.rs:
---
+++ target/debug/deps/install-e4385370da380117.d
@@ -0,0 +1,7 @@
+/home/kayla/Documents/GitHub/IdiotInstaller/target/debug/deps/libinstall-e4385370da380117.rmeta: src/main.rs src/command_funcs.rs src/constant_vars.rs
+
+/home/kayla/Documents/GitHub/IdiotInstaller/target/debug/deps/install-e4385370da380117.d: src/main.rs src/command_funcs.rs src/constant_vars.rs
+
+src/main.rs:
+src/command_funcs.rs:
+src/constant_vars.rs:
